ae2f1f96dbde2035f3ac33f14d2bd272f8f60e3c
AkashNegi1 <akashnegi977@gmail.com>
2026-04-14 16:41:38 +0530

    Fixed redis connection with TLS and auth

--- src/queue/video.queue.ts
+++ src/queue/video.queue.ts
@@ -1,8 +1,13 @@
-import { Queue } from "bullmq"
+import { Queue } from 'bullmq';
 
-export const videoQueue = new Queue("video-processing", {
-  connection:{
-    host: process.env.REDIS_HOST ,
+export const videoQueue = new Queue('video-processing', {
+  connection: {
+    host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    tls: {
+      rejectUnauthorized: false,
+    },
   },
-})
+});
